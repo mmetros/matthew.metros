5ce6af80dd1431c9d8d9ce50e3df20602e2a995a
Matthew <mmetros@wesleyan.edu>
2018-10-11 00:02:21 -0400

    files and base html setup

--- code/app.py
+++ code/app.py
@@ -1,9 +1,10 @@
 from myproject import app
-from flask import render_template
+from flask import Flask, render_template
 
-app.route('/')
+@app.route('/')
 def index():
-    return render_template('home.html')
+    heading = "How Are We Today?"
+    return render_template('home.html', heading=heading)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
